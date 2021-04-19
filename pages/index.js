e903a33e9dc6f06ef01eafc5a571881b9da65ec6
SoniaIsMad <manoubisonia@gmail.com>
2021-04-19 17:10:26 +0200

    first draft

--- pages/index.js
+++ pages/index.js
@@ -3,62 +3,37 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} >
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          John Doe
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <h2>Front End Engineer</h2>
+        <div className="flex flex-row justify-between w-2/5">
+            <a href="#">
+              <img className="h-12 mt-5" src="/twitter.png" alt="twitter icon" />
+            </a>
+            <a href="#">
+              <img className="h-12 mt-5" src="/linkedin.png" alt="linkedin icon" />
+            </a>
+            <a href="#">
+              <img className="h-12 mt-5" src="/mail.png" alt="mail icon" />
+            </a>
+            <a href="#">
+              <img className="h-12 mt-5" src="/github.png" alt="github icon" />
+            </a>
         </div>
+
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+          Made with Love, Next.js and Tailwind {' '}
+          <img src="/heart.svg" alt="Heart Logo" className={styles.logo} />
       </footer>
     </div>
   )
